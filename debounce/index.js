ca03d17409e906ec44c38e7e7b0e4c3444c34f04
Chavda Himanshu H <himanshuisherenow@gmail.com>
2023-12-12 01:46:22 +0530

    throttlin

--- debounce/index.js
+++ debounce/index.js
@@ -11,6 +11,32 @@
 
 // console.log(v)
 
+// Throttle function
+// function throttle(func, delay) {
+//   let lastCall = 0;
+
+//   return function() {
+//     const now = Date.now();
+
+//     if (now - lastCall >= delay) {
+//       func.apply(this, arguments);
+//       lastCall = now;
+//     }
+//   };
+// }
+
+// // Example function to be throttled
+// function handleScroll() {
+//   console.log('Scrolled!');
+//   // Your actual code to handle the scroll event goes here
+// }
+
+// // Throttle the function using the throttle utility
+// const throttledHandleScroll = throttle(handleScroll, 1000); // 1000ms (1 second) delay
+
+// // Attach the throttled function to the scroll event
+// window.addEventListener('scroll', throttledHandleScroll);
+
 
 function debounce(func, delay) {
   let timeoutId;
